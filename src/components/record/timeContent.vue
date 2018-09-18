6a44b4fb339676e6703f816f372f50d1bf3dc170
songpeilann <songpeilan@hefantv.com>
2018-09-18 18:33:44 +0800

    update web

--- src/components/record/timeContent.vue
+++ src/components/record/timeContent.vue
@@ -30,28 +30,39 @@
                         flag: 2
                     },
                 ],
-                box: [
+               /* box: [
                     {id: 1, text: 's睡啦那s睡啦那几s睡啦那几s', flag: 0, indexes: [0]},
                     {id: 2, text: 's睡啦那几', flag: 1, indexes: [1, 2, 3, 4]},
                     {id: 3, text: 's睡啦那几', flag: 2, indexes: [5, 6, 7, 8]}
-                ],
+                ],*/
                 height: 25,
 
             }
         },
         methods: {},
-        mounted(){
-            let box = []
-            for(let a=0;a<36;a++){
-                box.push({text: '', flag: 0, indexes: [a]})
+        computed:{
+            box:function(){
+                let box = []
+                for(let a=0;a<36;a++){
+                    box.push({text: '', flag: 0, indexes: [a]})
+                }
+                this.content.map((obj)=>{
+                    let startIndex = obj.indexes[0]
+                    box.splice(startIndex,obj.indexes.length,obj)
+                })
+                return  box
             }
-            this.content.map((obj)=>{
-                let startIndex = obj.indexes[0]
-                box.splice(startIndex,obj.indexes.length,obj)
-            })
+        },
+        mounted(){
 
-            this.box = box
-
+            setTimeout(()=>{
+                this.content.push({
+                    id: 2,
+                    indexes: [13,14,15],
+                    text: "睡懒觉33",
+                    flag: 2
+                })
+            },2000)
 
         },
         watch: {}
